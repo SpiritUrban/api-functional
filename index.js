ed05e24a4cfddd697b4998fcc36c16c6aed8689a
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2024-05-12 15:33:48 +0300

    one server 8000

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 import http from 'http';
 
-const server = http.createServer((req, res) => {
-    console.log('req url', req.url);
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<a href="/x">Link</a>');
-});
-
-server.listen(3001, () => {
-    console.log('Server running at http://localhost:3001/');
-});
-
-
-
 const server2 = http.createServer((req, res) => {
     console.log('req url', req.url);
 
